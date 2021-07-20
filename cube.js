48fb9b7e5cdcefa24c27a4a0d9574cde2478682e
Isaiah Odhner <isaiahodhner@gmail.com>
2021-07-19 20:46:27 -0400

    Handle window resize for controls

--- cube.js
+++ cube.js
@@ -411,6 +411,7 @@ function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize(window.innerWidth, window.innerHeight);
+	controls.handleResize();
 }
 
 function animate() {
